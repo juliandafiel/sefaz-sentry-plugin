2a8ee1d37c5d52a32d5ac4358d509bc558195570
Jaco Freire <juliandafiel2@gmail.com>
2026-06-27 11:49:34 -0300

    Corrige caminho do IntelliJ para 2026.1.3

--- listar-jars-intellij.bat
+++ listar-jars-intellij.bat
@@ -3,17 +3,17 @@ setlocal
 rem ============================================================
 rem  Lista os JARs da plataforma IntelliJ instalada localmente.
 rem  Uso:
-rem    listar-jars-intellij.bat "C:\Program Files\JetBrains\IntelliJ IDEA 2026.1"
+rem    listar-jars-intellij.bat "C:\Program Files\JetBrains\IntelliJ IDEA 2026.1.3"
 rem  (se nao passar o caminho, usa o default abaixo)
 rem ============================================================
 
 set "IDEA=%~1"
-if "%IDEA%"=="" set "IDEA=C:\Program Files\JetBrains\IntelliJ IDEA 2026.1"
+if "%IDEA%"=="" set "IDEA=C:\Program Files\JetBrains\IntelliJ IDEA 2026.1.3"
 
 if not exist "%IDEA%\lib" (
     echo [ERRO] A pasta "%IDEA%\lib" nao existe.
     echo Passe o caminho da sua IntelliJ, por exemplo:
-    echo     listar-jars-intellij.bat "C:\Program Files\JetBrains\IntelliJ IDEA 2026.1"
+    echo     listar-jars-intellij.bat "C:\Program Files\JetBrains\IntelliJ IDEA 2026.1.3"
     echo Dica: IntelliJ ^> Help ^> About mostra a versao; ou clique direito no atalho ^> Abrir local do arquivo.
     exit /b 1
 )
